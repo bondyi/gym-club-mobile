e38c32ee19564c252e5e35cd4676e85c771d7ab4
Ilya Bondarik <il.bandaryk@gmail.com>
2025-11-08 19:15:12 +0300

    refactor: await async init, navigation and page transitions

--- lib/src/on_boarding/presentation/widgets/on_boarding_body.dart
+++ lib/src/on_boarding/presentation/widgets/on_boarding_body.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:gym_club_mobile/core/common/widgets/custom_elevated_button.dart';
 import 'package:gym_club_mobile/core/extensions/context_extension.dart';
+import 'package:gym_club_mobile/l10n/app_localizations.dart';
 import 'package:gym_club_mobile/src/on_boarding/domain/entities/page_content.dart';
 import 'package:gym_club_mobile/src/on_boarding/presentation/cubit/on_boarding_cubit.dart';
 
@@ -40,8 +40,8 @@ class OnBoardingBody extends StatelessWidget {
               SizedBox(height: context.height * .25),
               CustomElevatedButton(
                 label: AppLocalizations.of(context)!.onBoardingButtonGetStarted,
-                onPressed: () {
-                  context.read<OnBoardingCubit>().cacheFirstTimer();
+                onPressed: () async {
+                  await context.read<OnBoardingCubit>().cacheFirstTimer();
                 },
               ),
             ],
